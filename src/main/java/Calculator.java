169577b6b866f7a6f95e48ee156b530eb7736b72
PoojaPatil3456 <poojapatil2402@gmail.com>
2021-08-21 22:47:14 +0530

    Calculator basic operations using mockito

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,23 @@
 public class Calculator {
+    ArithmeticOperations service;
+    public Calculator(ArithmeticOperations service) {
+        this.service = service;
+    }
+
+    public double sum(double input1, double input2) {
+        return service.sum(input1,input2);
+    }
+
+    public double difference(double input1, double input2) {
+        return service.difference(input1,input2);
+    }
+
+    public double product(double input1, double input2) {
+        return service.product(input1,input2);
+    }
+
+    public double division(double input1, double input2) {
+        return service.division(input1,input2);
+    }
 }
+
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,41 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class CalculatorTest {
+    Calculator calc = null;
+    ArithmeticOperations service = mock(ArithmeticOperations.class);
+    @BeforeEach
+    void initiateObject(){
+        calc = new Calculator(service);
+    }
+
+    @Test
+    void toCalculateSumOfTwoDoubleValues(){
+        when(service.sum(3.0,7.0)).thenReturn(10.0);
+        assertEquals(10.0,calc.sum(3.0,7.0));
+        verify(service).sum(3.0,7.0);
+    }
+
+    @Test
+    void toCalculateDifferenceBetweenTwoDoubleValues(){
+        when(service.difference(10.0,2.0)).thenReturn(5.0);
+        assertEquals(5.0,calc.difference(10.0,2.0));
+        verify(service).difference(10.0,2.0);
+    }
+
+    @Test
+    void toCalculateProductOfTwoDoubleValues(){
+        when(service.product(5.0,3.0)).thenReturn(15.0);
+        assertEquals(15.0,calc.product(5.0,3.0));
+        verify(service).product(5.0,3.0);
+    }
+
+    @Test
+    void toCalculateQuotientWhenDividingTwoDoubleValues(){
+        when(service.division(12.0,2.0)).thenReturn(6.0);
+        assertEquals(6.0,calc.division(12.0,2.0));
+        verify(service).division(12.0,2.0);
+    }
 }
